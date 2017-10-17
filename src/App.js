74e52a3ef07f8bf189a0693628c146cf11326145
Jonas Lindquist <jonas.lindquist@tui.se>
2017-10-17 21:52:02 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to TUI</h1>
         </header>
         <p className="App-intro">
-          Hello Amazon!
+          Hello Amazon Web Services!
         </p>
       </div>
     );
